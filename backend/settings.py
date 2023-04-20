16e757b0e3d477032c53097f5134046c2c16a16e
Zaid <karaymehzaid@gmail.com>
2023-04-20 15:02:19 +0300

    logo

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [str(host) for host in os.environ['ALLOWED_HOSTS'].split(',')] if os.environ['ALLOWED_HOSTS'] else ["*"]
 
